c16d9330cd5063d1fab4150a28c7c059105e0861
Kaxa Cxomelidze <kaxacxomelidze@gmail.com>
2026-05-12 02:11:14 +0400

    debug: surface crash error message in response

--- api/index.js
+++ api/index.js
@@ -1,2 +1,13 @@
 import handler from '../server.js';
-export default handler;
+
+export default async function (req, res) {
+  try {
+    await handler(req, res);
+  } catch (err) {
+    console.error('[CRASH]', err?.message, err?.stack);
+    if (!res.headersSent) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error: ' + err?.message + '\n\n' + err?.stack);
+    }
+  }
+}
